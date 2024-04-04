e4f9eb179a3a608ff33ce87ef959a5750a90d19a
HarshAhir04 <ahirharsh18128@gmail.com>
2024-04-04 08:52:14 +0530

    this is the third advanced javascript series file to convert the number to string and complex console log prints

---
+++ 03_StringtoNumber.js
@@ -0,0 +1,16 @@
+console.log("hello Advanced JavaScript");
+
+console.log(1 + 2 + "3");
+console.log("1" + 2 + 3);
+
+console.log(+true);
+console.log(+"");
+console.log(+null);
+console.log(+undefined);
+console.log(+NaN);
+console.log(+0);
+console.log(+Infinity);
+console.log(-Infinity);
+console.log(-0);
+console.log(-NaN);
+console.log(-1);
