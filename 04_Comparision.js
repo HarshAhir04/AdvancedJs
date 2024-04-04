9244f56f2bd013869a46c29e3d3998cd9d5ba31d
HarshAhir04 <ahirharsh18128@gmail.com>
2024-04-04 10:26:57 +0530

    for the advanced javascript series forth file to learn about the javascript comparision

---
+++ 04_Comparision.js
@@ -0,0 +1,40 @@
+//in this file we will learn about comparision operators
+console.table("comparision operators");
+console.log("comparision operators");
+
+console.log(1 == 1);
+console.log(1 == 2);
+
+//questionable comperision 
+console.log(1 == "1");
+console.log("1" == 1);
+
+//for null
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null != 0);
+console.log(null < 0);
+console.log(null != 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null > 0);
+console.log(null < 0);
+
+//for undefined
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined != 0);
+console.log(undefined < 0);
+console.log(undefined != 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//for === operator 
+console.log("1" === 1);
+console.log(1 === 1);
