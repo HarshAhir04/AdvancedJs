f403d10fdc4d68be27077cf3b635552f679b4e3e
Harsh Ahir <161831064+HarshAhir04@users.noreply.github.com>
2024-04-12 10:07:11 +0530

    convert the date in to the full month and a full year

--- 08_Date_and_Time.js
+++ 08_Date_and_Time.js
@@ -19,10 +19,10 @@ console.log(mydate.getTime());
 
 console.log(typeof mydate);
 
-let created_date = new Date(2023,0,23);
+let created_date = new Date(2023, 0, 23);
 console.log(created_date.toDateString());
 
-let created_date2 = new Date(2023,0,23,11,20,30,0);
+let created_date2 = new Date(2023, 0, 23, 11, 20, 30, 0);
 console.log(created_date2.toLocaleString());
 
 let created_date3 = new Date("04-09-2023");
@@ -34,7 +34,7 @@ console.log(created_date4);
 
 console.log(typeof created_date4);
 // console.log(created_date4.getTime());
-console.log(Math.floor(Date.now()/1000));
+console.log(Math.floor(Date.now() / 1000));
 
 //Another date methods to be useds
 let newdate = new Date();
@@ -48,3 +48,34 @@ console.log(newdate.toLocaleString("default", {
     month: "long",
     day: "numeric"
 }));
+
+let new_date = new Date(11-2-2023);
+console.log(new_date);
+
+console.log(new_date.toDateString());
+console.log(new_date.toLocaleDateString());
+console.log(new_date.getFullYear());
+// console.log(new_date.getUTCSeconds);
+
+// function getFullMonthName(date) {
+//     // Use toLocaleString with options for month format
+//     const fullMonth = date.toLocaleString('default', { month: 'long' });
+//     return fullMonth;
+//   }
+
+//   // Example usage
+//   const today = new Date();
+//   const fullMonth = getFullMonthName(today);
+
+//   console.log(fullMonth);
+
+function getFullMonthName(date) {
+    // Use toLocaleString with options for month format
+    const fullMonth = date.toLocaleString('default', { month: 'long' });
+    return fullMonth;
+}
+const today = new Date();
+const fullMonth = getFullMonthName(today);
+
+console.log(fullMonth);
+
